488551de0dadb4f8909f18c2f9f234be0fd19234
RoseDzo <rdzotor@st.ug.edu.gh>
2024-05-18 14:54:16 +0000

    task 1 and 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,6 +1,20 @@
-function processArray(arr){
+function processArray(arr) {
   return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
-
 }
-const result = processArray([1, 2, 5, 6, 9])
-console.log(result)
+
+function formatArrayStrings(stringArray, numberArray) {
+  const result = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    if (numberArray[i] % 2 === 0) {
+      result.push(stringArray[i].toUpperCase());
+    } else {
+      result.push(stringArray[i].toLowerCase());
+    }
+  }
+  return result;
+}
+const numbers = [4, 1, 8, 7, 10];
+const processedNumbers = processArray(numbers);
+const strings = ['ramen', 'teddy', 'bear', 'koala', 'lizard'];
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+console.log(formattedStrings);
